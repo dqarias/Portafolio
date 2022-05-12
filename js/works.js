946b3c46b41b8023db75fcf1c33fe33a98ae3887
dqarias <dino.quispea@gmail.com>
2022-05-11 21:50:07 -0500

    Add script file for works section

---
+++ js/works.js
@@ -0,0 +1,14 @@
+/*const openModal = document.querySelector('.open-modal')
+const modal =document.querySelector('.modal');
+const closeModal = document.querySelector('.modal-close')
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show')
+})
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show')
+})*/
+
