efef03c390fd7030e60556c9a1e3ceecfef52b42
dqarias <dino.quispea@gmail.com>
2022-05-14 10:27:36 -0500

    Update style in form.css file

--- js/validation.js
+++ js/validation.js
@@ -3,6 +3,7 @@ const email = document.getElementById('email').value;
 if (email !== email.toLowerCase()) {
     event.preventDefault();
     const emailError = document.getElementById('error-message');
+    emailError.innerText = "Email should be in lower case"
     emailError.classList.add('active');
 }
 });
